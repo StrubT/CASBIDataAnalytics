26e12d155f27b4830bb7a0ec5983813715e1da82
Thomas Reto Strub <ths@strubt.ch>
2016-12-14 14:07:08 +0100

    add workshop 2

---
+++ workshop-2.r
@@ -0,0 +1,31 @@
+
+PATH <- 'D:/mse/BI/02_data-analytics/03_data/WORLD.CSV'
+
+WORLD <- read.csv(PATH)
+
+### METRISCH ###
+
+WORLD_MET <- WORLD[c(5,6,8,9,10,12)]
+names(WORLD_MET)
+summary(WORLD_MET)
+
+#...
+
+### KATEGORIAL ###
+
+WORLD_KAT <- WORLD[c(16:19)]
+names(WORLD_KAT)
+summary(WORLD_KAT)
+
+length(table(WORLD_KAT$GOVERN))
+names(which.max(table(WORLD_KAT$GOVERN)))
+
+length(table(WORLD_KAT$GOVERN_S))
+names(which.max(table(WORLD_KAT$GOVERN_S)))
+
+length(table(WORLD_KAT$RELIGION))
+names(which.max(table(WORLD_KAT$RELIGION)))
+
+length(table(WORLD_KAT$RELIGION_S))
+names(which.max(table(WORLD_KAT$RELIGION_S)))
+
