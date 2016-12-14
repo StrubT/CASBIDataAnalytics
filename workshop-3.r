a8a5f6d8119c3b451ed8b8481b7386ea954e5fba
Thomas Reto Strub <ths@strubt.ch>
2016-12-14 14:07:23 +0100

    add workshop 3

---
+++ workshop-3.r
@@ -0,0 +1,20 @@
+
+PATH <- 'D:/mse/BI/02_data-analytics/03_data/WORLD.CSV'
+
+WORLD <- read.csv(PATH)
+WORLD. <- WORLD[c(5,6,8,9,10,12,16:19)]
+
+COR <- cor(WORLD.[c(1:6)], use = 'pairwise')
+COR
+
+library(corrplot)
+corrplot(COR, type = 'lower')
+
+library(corrgram)
+corrgram(COR, upper.panel = NULL)
+
+plot(WORLD.)
+
+library(ggplot2)
+library(GGally)
+ggpairs(WORLD.)
