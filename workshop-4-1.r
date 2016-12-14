3167838ed84bc008d6285f8ef9ca47d1605fe39a
Thomas Reto Strub <ths@strubt.ch>
2016-12-14 14:07:33 +0100

    add workshop 4

---
+++ workshop-4-1.r
@@ -0,0 +1,23 @@
+
+PATH <- 'D:/mse/BI/02_data-analytics/03_data/AUTOPREISE.CSV'
+
+AP <- read.csv(PATH)
+
+with(AP, plot(KW, VERBR))
+
+AP_FORMULA <- VERBR ~ KW
+
+AP_MODEL <- lm(AP_FORMULA, AP)
+AP_MODEL
+summary(AP_MODEL)
+
+abline(AP_MODEL, lwd = 2, col = 'red')
+plot(AP_MODEL)
+
+VERBR_PREDICTION <- predict(AP_MODEL, AP)
+head(data.frame(AP$VERBR, VERBR_PREDICTION))
+
+plot(AP$VERBR, VERBR_PREDICTION,
+     xlim = c(0, 30),
+     ylim = c(0, 30))
+abline(y~x, coef = c(0, 1))
---
+++ workshop-4-2.r
@@ -0,0 +1,18 @@
+
+PATH <- 'D:/mse/BI/02_data-analytics/03_data/AUTOPREISE.CSV'
+
+AP <- read.csv(PATH)
+AP <- subset(AP, PREIS < 200000)
+
+AP_FORMULA <- PREIS ~ KW + BESCHLEU + HOECHSTG + VERBR + HUBRAUM
+AP_MODEL <- lm(AP_FORMULA, AP)
+AP_STEP <- step(AP_MODEL)
+
+summary(AP_MODEL)
+summary(AP_STEP)
+
+PREIS_PREDICTION <- predict(AP_MODEL, AP)
+head(data.frame(AP$PREIS, PREIS_PREDICTION))
+
+plot(AP$PREIS, PREIS_PREDICTION)
+abline(y~x, coef = c(0, 1))
