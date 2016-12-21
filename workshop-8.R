1ea387fb90cb1bfcb853344bea156afaa2a3f297
Thomas Reto Strub <ths@strubt.ch>
2016-12-21 13:58:22 +0100

    fix workshop 8

--- workshop-8.R
+++ workshop-8.R
@@ -2,15 +2,16 @@
 PATH <- 'D:/mse/BI/02_data-analytics/02_workshop/00_data/WINE.CSV'
 
 WINE <- read.csv(PATH)
+WINE <- scale(WINE[2:14])
 
-WINE_PC <- prcomp(WINE[2:14])
+WINE_PC <- prcomp(WINE)
 
 biplot(WINE_PC)
 
 PRED <- predict(WINE_PC)
 plot(PRED)
 
-WINE_COR <- cor(cbind(WINE[2:14], WINE_PC$x[,1:2]))
+WINE_COR <- cor(cbind(WINE, WINE_PC$x[,1:2]))
 
 library(corrplot)
 corrplot(WINE_COR)
